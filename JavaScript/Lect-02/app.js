cde89640f755e3811d43096a585959a50265004a
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-04-12 18:19:17 +0530

    Add

---
+++ JavaScript/Lect-02/app.js
@@ -0,0 +1,21 @@
+console.log("hello world!") 
+  
+let a = 20; 
+let b = 5; 
+console.log("sum of :", a + b);
+console.log("sub is :", a - b);
+console.log("mul is :", a * b);
+console.log("div is :", a / b); 
+
+let pencilPrice = 10; 
+let erasorPrice = 5; 
+// let output = "The total price is : " + (pencilPrice + erasorPrice) + " rupees."; 
+let output = `The total price is : ${pencilPrice + erasorPrice} rupees`;
+console.log(output);
+ 
+
+
+
+
+
+
