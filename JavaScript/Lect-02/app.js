284483503ae1df3d56918e66ff0b3b5f2eba3f5e
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-04-12 19:38:33 +0530

    Add

--- JavaScript/Lect-02/app.js
+++ JavaScript/Lect-02/app.js
@@ -1,18 +1,48 @@
-console.log("hello world!") 
+// console.log("hello world!") 
   
-let a = 20; 
-let b = 5; 
-console.log("sum of :", a + b);
-console.log("sub is :", a - b);
-console.log("mul is :", a * b);
-console.log("div is :", a / b); 
+// let a = 20; 
+// let b = 5; 
+// console.log("sum of :", a + b);
+// console.log("sub is :", a - b);
+// console.log("mul is :", a * b);
+// console.log("div is :", a / b); 
 
-let pencilPrice = 10; 
-let erasorPrice = 5; 
-// let output = "The total price is : " + (pencilPrice + erasorPrice) + " rupees."; 
-let output = `The total price is : ${pencilPrice + erasorPrice} rupees`;
-console.log(output);
+// let pencilPrice = 10; 
+// let erasorPrice = 5; 
+// // let output = "The total price is : " + (pencilPrice + erasorPrice) + " rupees."; 
+// let output = `The total price is : ${pencilPrice + erasorPrice} rupees`;
+// console.log(output);
+  
+// Unary operators 
+
+// let a = 5;  
+
+// console.log(a++);   // 5 + 1 = 6  but print 5 
+// console.log(++a);  // 1 + 6 = 7 
+
+// console.log(a--);   // 7 - 1 = 6 
+// console.log(--a);  //  6 - 1 = 5  
  
+
+// // compersion 
+// let value = 40; 
+// console.log(value>40);  // false  
+// console.log(value >= 40);  // true 
+
+
+// Conditional Statements  
+
+let a = 30; 
+if( a > 18 ) {
+  console.log("adult"); 
+} else {
+  console.log("chhota bcha ");
+}
+
+
+
+
+
 
 
 
