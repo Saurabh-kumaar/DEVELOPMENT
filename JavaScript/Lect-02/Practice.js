5a41f370cbb27d55e9648dd914d5119b111153af
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-04-13 08:57:22 +0530

    Add

--- JavaScript/Lect-02/Practice.js
+++ JavaScript/Lect-02/Practice.js
@@ -1,8 +1,68 @@
-let light = red , yellow , green; 
-if( traffic =  zam){  
-  console.log
 
-} ;lkok 
+// Qs .1 
+// create traffic color 
+
+// let color = "red"; 
+// if(color == "red"){  
+//   console.log("stop")
+// }  
+// if(color == "yellow"){
+//   console.log("slow Down")
+// } 
+// if(color == "green"){
+//   console.log("Go")
+// }  
+ 
+
+
+// ----------- 
+
+// Qs.2 create a system to calculate popcorn prices based on the size customer asked for: 
+   
+// let size = "XL"; 
+
+// if(size === "XL"){
+//   console.log("price is Rs. 250"); 
+// } else if(size === "L"){
+//   console.log("price is Rs. 200"); 
+// } else if(size === "M"){
+//   console.log("price is Rs. 100"); 
+// } else {
+//   console.log("price is Rs. 50"); 
+// }
+
+
+// Q . 3 :- a good string and string length should be greater than 3 then-
+
+// let str = "alright"; 
+// if(str[0] === "a" && str.length > 3){
+//   console.log("good string")
+// } else {
+//   console.log("bad string")
+// }
+
+ 
+// output predict :- 
+
+let num = 12; 
+
+if((num%3 === 0) && ( (num+1 == 15) || (num-1 == 11))) {
+  console.log("safe");   // (true && ((false || true = true)) = true && true = true then safe is right ans.
+} else {
+  console.log("unsafe");
+} 
+
+
+
+
+ 
+
+
+
+  
+
+
+
 
 
 
--- JavaScript/Lect-02/app.js
+++ JavaScript/Lect-02/app.js
@@ -32,14 +32,82 @@
 
 // Conditional Statements  
 
-let a = 30; 
-if( a > 18 ) {
-  console.log("adult"); 
-} else {
-  console.log("chhota bcha ");
-}
+// let age = 23; 
+// if(age >= 18){
+//   console.log("you can vote")
+// } else (
+//   console.log("you can't vote")
+// ) 
 
-// console.log("saurabh ");
+// -------- 
+
+// let a = 15; 
+// if( a > 18 ) {
+//   console.log("adult"); 
+// } else if (a < 18 ){
+//   console.log(" medium chhota bcha ");
+// } else if ( a < 12 ){
+//   console.log("jada chhota ") 
+// }
+
+// ----------- 
+
+// let marks = 44;
+// if(marks >= 80 ){
+//   console.log("A++");
+// } else if(marks >= 60 ){
+//   console.log("A");
+// } else if(marks >= 30){
+//   console.log("B");
+// } else if(marks <= 30) {
+//   console.log("fail"); 
+// }
+ 
+// -------------
+// nested loop 
+
+// let marks = 77; 
+
+// if(marks >= 33) {
+//   console.log("Pass");  
+//   if( marks >= 80){
+//     console.log("Grade : 0"); 
+//   } else {
+//     console.log("Grade : A")
+//   }
+// } else {
+//   console.log("Better luck next time!"); 
+// }
+
+// ----------- 
+  // && AND operator 
+
+// let marks = 89; 
+
+// if(marks >= 33 && marks >= 80){
+//   console.log("pass"); 
+//   console.log("A+")   // if both true then output come other than no output 
+// }
+  
+
+// or --- 
+// let marks = 40; 
+
+// if(marks >= 33 || marks >= 80) {  // if one true then output come other than also come 
+//   console.log("pass");
+//   console.log("A++");
+// }
+  
+// logical --------- 
+
+let marks = 40; 
+
+if(!(marks < 33)) {    
+  console.log("pass");
+  console.log("A++");
+}
+ 
+
 
 
 
