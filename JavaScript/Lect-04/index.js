fa2bd9e9fbc44ee9da02f3c5fdbe1af0859bfd34
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-04-17 22:16:25 +0530

    Add

--- JavaScript/Lect-04/index.js
+++ JavaScript/Lect-04/index.js
@@ -25,12 +25,12 @@
  
 // find any num of table ------
 
-let n = prompt("write your number");  
-n = parseInt(n); 
+// let n = prompt("write your number");  
+// n = parseInt(n); 
 
-for(let i=n; i<=n*10; i=i+n){
-  console.log(i); 
-}
+// for(let i=n; i<=n*10; i=i+n){
+//   console.log(i); 
+// }
 
 // ---- 
  
@@ -40,6 +40,121 @@ for(let i=n; i<=n*10; i=i+n){
 // }
  
 // ------ 
+
+// nestes looop 
+
+// for(let i=1; i<=3; i++){
+//   console.log(`root loop ${i}`); 
+//   for(let j=1; j<=3; j++){
+//     console.log(j); 
+//   } 
+// }
+
+// ------------
+
+// while loop  -------- 
+
+// let i=0; 
+// while(i<=20){
+//   console.log(i); 
+//   i++; 
+// }
+
+// --- 
+
+// let i=5; 
+// while(i>=1){
+//   console.log(i); 
+//   i--; 
+// }
+
+// even 
+// let i=2; 
+// while(i<=15){
+//   console.log(i); 
+//   i = i+2; 
+// }
+
+// ------------------------------- 
+ 
+// const favMovie = "avatar"; 
+
+// let guess = prompt("guess my fav movie"); 
+// while( (guess != favMovie) && (guess != "quit")) {
+//    guess = prompt("please try again"); 
+// }
+
+// if(guess == favMovie) {
+//   console.log("congrats you did ava!!"); 
+// } else {
+//   console.log("you can quit"); 
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
