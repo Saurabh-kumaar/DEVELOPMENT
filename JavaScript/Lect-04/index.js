50cb49a2b5dc10637e2b4ccefc0c4ecf22248b94
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-04-19 23:30:01 +0530

    Add

--- JavaScript/Lect-04/index.js
+++ JavaScript/Lect-04/index.js
@@ -90,15 +90,130 @@
 //   console.log("you can quit"); 
 // }
 
+// ---------------------------- 
+
+  // break - keyword 
+
+// let i=1; 
+// while(i<=5){ 
+//   if(i == 3){  // for break in between
+//     break;
+//   }
+//   console.log(i); 
+//   i++; 
+// }  
+
+// ------------------------------ 
+
+// Loops with Arrays :- 
+
+// let fruits = ["kela", "sev", "aam", "amrud", "angur"]; 
+// fruits.push("pineapple");  
+// for(let i=0; i<fruits.length; i++){
+//   console.log(i,fruits[i]);  
+
+// }
+
+// ---------------------------------------
+ 
+// Nested loop with nested arrays :- 
 
 
 
+// let players = [ [ "Hardik", "Rohit", "chokali"], ["Head", "smith","krishgell"]]; 
+
+// for(let i=0; i<players.length; i++){
+//   console.log(i,players[i], players[i].length); 
+//   for(let j=0; j<players[i].length; j++){
+//     console.log(`j=${j}, ${players[i][j]}`); 
+//   }
+// }
+ 
+
+// ------ 
+
+// let game = [["cricket","chess","football"],["bollyball","kabbadi","kho-kho"]]; 
+
+// for(let i=0; i<game.length; i++){
+//   console.log(i,game[i],game[i].length); 
+//   for(let j=0; j<game[i].length; j++){
+//     console.log(`j=${j}, ${game[i][j]}`);
+//   }
+// }
+ 
+// ---------- 
+
+// let state = [["bihar","chandigarh","up"],["uttrakhand","punjab","sikkim"]]; 
+
+// console.log(state[0][0]);  
+// console.log(state[0][1]);  
+// console.log(state[0][2]);  
+// console.log(state[1][0]);  
+// console.log(state[1][1]);  
+// console.log(state[1][2]);   
+
+// console.log(state[0]); 
+// console.log(state[1]);  
 
 
+// ----- 
+
+// let results = [["rinchu",99], ["mohit",99.9],["gautam",90]];  
+// console.log(results[2][0]); 
+ 
+// ------ 
+
+// let food = [["litti",30],["chokha",20],["puri",12],["sbji",10],["dal",9],["chaval",13]];  
+
+// for(let i=0; i<food.length; i++){ 
+//   for(let j=0; j<food[i].length; j++){
+//     // console.log(food[i][j]);   
+//     console.log(food[3][1])
+//   }
+// }
+ 
+// ------- 
+// ----------- for of loop --------------- 
 
 
+// let fruits = [ "mango", "banana", "apple", "grapes"]; 
 
+// for(fruit of fruits) {
+//   console.log(fruit); 
+// }
 
+// ------- 
+
+// let players = ["pandya", "chokali","raina","gambhir","dhoni"]; 
+
+// for(i of players) {
+//   console.log(i); 
+// }
+ 
+
+// --------  nested for-of loop ------------- 
+
+// let coding = [[ "java", "python", "c", "c++"],[ "dev", "dsa" , "golang"]]; 
+
+// for(lang of coding){
+//   for(master of lang){
+//     console.log(master); 
+
+//   }
+// }
+
+// -------- 
+
+// let principal = [["english", "math", "compluter", "development"], ["dsa", "ms.in tech","full stack"]]; 
+
+// for(masters of principal) {
+//   for(students of masters){
+//     console.log(students,students.length); 
+//   }
+// }
+
+let num = [1,2,3,4,5,6,2,3]; 
+console.log(num.slice[1]); 
 
 
 
