49b6638c4de41af07a9e03ad9bafe7e7594b585e
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-04-24 17:36:00 +0530

    Add

---
+++ JavaScript/Lect-05/index.js
@@ -0,0 +1,132 @@
+
+    // creating objects 
+
+// students = {
+//   name: "mohit",
+//   age: 20
+// }
+
+// const item = {
+//   price: 20,
+//   discount: 5,  
+//   colores: ["pink", "red"]
+// } 
+// -------------- 
+
+    // creating post 
+
+// const post = {
+//   username: "@saurabh123", 
+//   content: "this is my #firstpost", 
+//   likes: 500, 
+//   repost: 135, 
+//   tags: "@twitter"
+// } 
+// ---------  
+    // get
+// let boys = {
+//   name: "devil", 
+//   marks: 94.3
+// };  
+// -----------
+
+          // Add / update values 
+
+// const student = {
+//   name: "devil", 
+//   age: 20, 
+//   marks: 90.2,
+//   city: "varanasi"
+// };
+// ------------ 
+ 
+            // Nested objects 
+
+// const school = {
+//   nitesh : {
+//     teach: "science", 
+//     city: "ramgarh"
+//   }, 
+
+//   avinash: {
+//      teach: "math",
+//      city: "patna"
+//   }, 
+
+//   rinchu : {
+//     teach: "Biology", 
+//     city: "varanasi"
+//   }
+// } 
+// ----------- 
+          // Array of objects 
+
+// const heroes = [ 
+//   { 
+//     name: "mohit",
+//     age: 23, 
+//     city: "ara"
+//   },
+//   { 
+//     name: "rohit", 
+//     age: 21, 
+//     city: "bhabhua"
+//   },
+//   { 
+//     name: "vikash", 
+//     age: 89, 
+//     city: "mohania"
+//   }
+// ] 
+// ------------ 
+
+// Math.abs(n)  - give positive no 
+// Math.pow(4 2); = 16 
+// Math.floor(n) = nearest , gretest, smallest 
+// Math.random(n) = give random values between 0-1 
+
+// ---------  
+
+      // create random values 
+// Math.random() 
+// Math.random() * 10 
+// Math.floor(Math.random() * 10 ) 
+// Math.floor(Math.random() * 10 ) + 1 
+ 
+// ------------- 
+ 
+
+
+
+
+
+
+
+
+
+
+// Practice questions ----------------- 
+
+// Generate a random number between 1 and 100; 
+
+// Math.random() 
+// Math.random() * 100 
+// Math.floor(Math.random() * 100 ) 
+// Math.floor(Math.random() * 100 ) + 1 
+// values get 
+ 
+// ------------ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
