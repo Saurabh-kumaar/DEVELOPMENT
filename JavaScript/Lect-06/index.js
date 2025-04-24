c514099f7feaf5f5e5d78daa06b510b2722d4e50
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-04-24 23:32:08 +0530

    Add

---
+++ JavaScript/Lect-06/index.js
@@ -0,0 +1,53 @@
+
+          // function 
+
+// function hello() { 
+//   console.log("hello bro kya hal hai");
+// } 
+
+// hello(); 
+// hello();
+// ----------- 
+
+// function printno() { 
+//   for(let i=1; i<=5; i++){
+//     console.log(i); 
+//   }
+// }
+// printno(); 
+
+// ------------------ 
+
+// function isAdult() {
+//   let age = 50;
+//   if(age > 18){
+//     console.log("it is adult"); 
+//   } else {
+//     console.log("it is not adult"); 
+//   }
+// }
+
+// isAdult();
+
+// ----------------
+
+        // function with arguments 
+
+// function printInfo(name, age) {
+//   console.log(`${name}'s"age is ${age}.`); 
+// } 
+
+// printInfo("rajat dalal", 23); 
+// printInfo("rajat dalal"); 
+// printInfo(23); 
+// ------------
+
+function sum(a, b){
+  console.log(a+b); 
+}
+
+sum(1,2); 
+sum(4.5,9);
+
+
+
---
+++ JavaScript/Lect-06/practice.js
@@ -0,0 +1,50 @@
+
+      // Practice Question 
+    // Qs 1:- 
+
+// function printpoem() {
+//   console.log("Twinkle Twinkle, little star"); 
+//   console.log("how i wonder what you are"); 
+
+// }
+
+// printpoem();   
+// ---------------
+
+// function findrandom() {
+//   let dice = Math.floor(Math.random() * 6 )+1; 
+//   console.log(dice); 
+// } 
+
+// findrandom(); 
+// findrandom(); 
+// findrandom(); 
+// findrandom(); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
