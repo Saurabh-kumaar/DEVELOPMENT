fa3a1dc6efa555f0ac141156ef875ebfcb929140
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-05-01 22:11:50 +0530

    Add

--- JavaScript/Lect-09/index.js
+++ JavaScript/Lect-09/index.js
@@ -115,7 +115,7 @@ input.setAttribute("placeholder", "username");
 let btn = document.querySelector("#btn"); 
 btn.classList.add("btnstyle");  
 
-// Qs 4:- df 
+// Qs 4:- df adf 
 
 let h1 = document.createElement("h1"); 
 h1.innerHTML = "<u>DOM Practice</u>";  
