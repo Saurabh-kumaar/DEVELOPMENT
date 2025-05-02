fcd9ad0d4e9dad435310e3acd29d6bf9a3e6be17
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-05-02 21:27:32 +0530

    Add

--- JavaScript/Lect-10/index.js
+++ JavaScript/Lect-10/index.js
@@ -1,31 +1,75 @@
 
 
-// let btn = document.querySelector("button"); // for single buttom
-let btns = document.querySelectorAll("button"); 
+            // mouse pointer 
 
-for(btn of btns) {
-  btn.onclick = sayHello;  
-  btn.onmouseenter = function () {
-    console.log("you entered a button"); // its only for cursor on botton not tap 
-  }
-  console.dir(btn); 
-}
-//  +
-function sayHello() {
-  alert("hello ji"); // when we tap then print. 
-}
+// // let btn = document.querySelector("button"); // for single buttom
+// let btns = document.querySelectorAll("button"); 
+
+// for(btn of btns) {
+//   btn.onclick = sayHello;  
+//   btn.onmouseenter = function () {
+//     console.log("you entered a button"); // its only for cursor on botton not tap 
+//   }
+//   console.dir(btn); 
+// }
+// //  +
+// function sayHello() {
+//   alert("hello ji"); // when we tap then print. 
+// }
  
  
+// ------------------------------------- 
+
+          // Event listeners 
+
+// let btns = document.querySelectorAll("button"); 
+
+// for(btn of btns) {
+//   // btn.onclick = sayHello; 
+
+//   //  btn.addEventListener("click", sayHello); 
+//   //  btn.addEventListener("click", sayName); 
+//    btn.addEventListener("dblclick", function () {
+//     console.log("you did double click")
+//    }); 
+//  }
+
+// function sayHello() {
+//   alert("hello!"); 
+// }
+ 
+// function sayName() { 
+//   alert("kya haal hai ji"); 
+// }
+
+// ===============================================
+
+        // Activity 
+
+// let btn = document.querySelector("button"); 
+
+// btn.addEventListener("click", function () { 
+//   let h3 = document.querySelector("h3"); 
+//   let randomColor = getRandomColor(); 
+//   h3.innerText = randomColor; 
+
+//   let div = document.querySelector("div"); 
+//   div.style.backgroundColor = randomColor; 
+  
+//   console.log("color updated"); 
+//  });
+
+// function getRandomColor() {
+//   let red = Math.floor(Math.random() * 255); 
+//   let green = Math.floor(Math.random() * 255); 
+//   let blue = Math.floor(Math.random() * 255); 
+
+//   let color = `rgb(${red}, ${green}, ${blue})`; 
+//   return color; 
+// }
 
 
-
-
-
-
-
-
-
-
+// ==================================================
 
 
 
