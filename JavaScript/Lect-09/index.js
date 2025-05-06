8dcc8614419584115f979ee95adc0f79d9792b33
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-05-06 08:43:25 +0530

    Add

--- JavaScript/Lect-09/index.js
+++ JavaScript/Lect-09/index.js
@@ -60,35 +60,41 @@
 // ======================================================= 
 
 
-    // Practice Questions :- 
+    // Practice Questions :-  
 
-    
-// let para = document.createElement('p'); 
-// para.innerText = "Hey Developers!"; 
-// document.querySelector('body').prepend(para); 
+// (I) :-
 
-// para.classList.add("red"); 
-// ---- 
+let para1 = document.createElement("p"); 
+para1.innerText = "Hey I'm red! "; 
+document.querySelector('body').append(para1);  
 
-// let h3 = document.createElement('h3'); 
-// h3.innerText = "Hey i'm a blue h3!"; 
-// document.querySelector('body').prepend(h3); 
+para1.classList.add('red'); 
+     
+// (ii):- 
 
-// h3.classList.add('blue'); 
+let h3 = document.createElement('h3'); 
+h3.innerText = "Im a blue h3"; 
+document.querySelector('body').append(h3); 
+h3.classList.add('blue'); 
+
+// (iii):- 
+
+let div = document.createElement("div"); 
+let h1 = document.createElement("h1"); 
+let para3 = document.createElement("p"); 
+
+h1.innerText = "I'm in a div"; 
+para3.innerText = "ME TOO!"; 
+
+div.append(h1); 
+div.append(para3); 
+div.classList.add("box"); 
+document.querySelector("body").append(div); 
 
 
-// let div = document.createElement('div'); 
-// let h1 = document.createElement('h1'); 
-// let para2 = document.createElement('para2'); 
 
-// h1.innerText = "I'm in a div"; 
-// para2.innerText = "Me TOO!!"; 
 
-// div.append(h1); 
-// div.append(para2); 
-// div.classList.add("box"); 
 
-// document.querySelector('body').prepend(div); 
 
 
 // ======================================================= 
--- JavaScript/Lect-10/index.js
+++ JavaScript/Lect-10/index.js
@@ -44,32 +44,114 @@
 
 // ===============================================
 
-        // Activity 
+        // Activity  
 
 // let btn = document.querySelector("button"); 
 
 // btn.addEventListener("click", function () { 
-//   let h3 = document.querySelector("h3"); 
-//   let randomColor = getRandomColor(); 
-//   h3.innerText = randomColor; 
+//         let h3 = document.querySelector("h3"); 
+//         let randomColor = getRandomColor(); 
+//         h3.innerText = randomColor;  
 
-//   let div = document.querySelector("div"); 
-//   div.style.backgroundColor = randomColor; 
-  
-//   console.log("color updated"); 
-//  });
+//         let div = document.querySelector("div"); 
+//         div.style.backgroundColor = randomColor; 
+
+//         console.log("color updated"); 
+// }); 
 
 // function getRandomColor() {
-//   let red = Math.floor(Math.random() * 255); 
-//   let green = Math.floor(Math.random() * 255); 
-//   let blue = Math.floor(Math.random() * 255); 
+//         let red = Math.floor(Math.random() * 255); 
+//         let green = Math.floor(Math.random() * 255); 
+//         let blue = Math.floor(Math.random() * 255); 
 
-//   let color = `rgb(${red}, ${green}, ${blue})`; 
-//   return color; 
+//         let color = `rgb(${red}, ${green}, ${blue})`;
+//         return color;  
 // }
 
+// ================================================== 
 
-// ==================================================
+                // addEventListener 
+                 
+// let p = document.querySelector("p"); 
+
+// p.addEventListener("click", function () {
+//         console.log("parah was clicked"); 
+// })
+// // --- 
+
+// let div = document.querySelector(".box");   
+// div.addEventListener("mouseenter", function() {
+//         console.log("para was clicked"); 
+// })
+
+// ------ 
+
+// let btn = document.querySelector("button"); 
+// let p = document.querySelector("p"); 
+// let h1 = document.querySelector("h1"); 
+// let h3 = document.querySelector("h3 "); 
+
+// function changeColor() {
+//         console.dir(this.innerText); 
+//         this.style.background = "red"; 
+// }
+ 
+// btn.addEventListener("click", changeColor); 
+// p.addEventListener("click", changeColor); 
+// h1.addEventListener("click", changeColor); 
+// h3.addEventListener("click", changeColor); 
+
+// button 
+// btn.addEventListener("click", function() {
+//         // console.log(this);
+//         // console.dir(this); 
+//         console.dir(this.innerText); 
+// })
+ 
+// ----------
+                // Keywords events 
+
+// let btn = document.querySelector("button"); 
+
+// btn.addEventListener("click", function (event) {
+//         console.log(event); 
+//         console.log("button clicked");  
+
+// });
+// ---
+// btn.addEventListener("dblclick", function (event) { 
+//           console.log(event); 
+//         console.log("button clicked"); 
+// }); 
+// ---------------------- 
+
+                // Keyword Events concept :- 
+
+
+// let inp = document.querySelector("input"); 
+
+// inp.addEventListener("keydown", function (event) {
+//         console.log("code = ", event.code);  // ArrowUp, ArrowDown, ArrowLeft, ArrowRight  
+//         if(event.code == "KeyU") {
+//                 console.log("character moves forward"); 
+//         } else if (event.code == "KeyD"){
+//                 console.log("character moves backward"); 
+//         } else if (event.code == "KeyL") {
+//                 console.log("character moves Left"); 
+//         } else if (event.code == "KeyR") {
+//                 console.log("character moves right!"); 
+//         }   
+//  });
+//  ----------------------- 
+
+        // Form events
+
+let form = document.querySelector("form"); 
+
+form.addEventListener("submit", function (event) {
+        // event.preventDefault();
+        alert("form submitted"); 
+}); 
 
 
 
@@ -80,5 +162,4 @@
 
 
 
-
-
+ 
