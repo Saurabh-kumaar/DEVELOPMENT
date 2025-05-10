e719a6ce969ffb64b1f87759bb906bb7a149ada9
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-05-10 15:23:26 +0530

    Add

--- JavaScript/Mini-project-js/index.js
+++ JavaScript/Mini-project-js/index.js
@@ -28,13 +28,30 @@ let ul = document.querySelector("ul");
 let inp = document.querySelector("input"); 
 
 
-btn.addEventListener("click", function () {
-  console.log(inp.value);  
-  inp.value = "";       // delete after input
-});
-jkljf 
+btn.addEventListener("click", function () { 
+  let item = document.createElement("li");  // for add task 
+  item.innerText = inp.value;  
+
+  // for delete button show
+  let delBtn = document.createElement("button"); 
+  delBtn.innerText = "delete"; 
+  delBtn.classList.add("delete"); 
+
+  item.appendChild(delBtn); 
+  ul.appendChild(item); 
+   inp.value = "";       // delete after input
+}); 
 
 
+ul.addEventListener("click", function (event) {
+  if(event.target.nodeName == "BUTTON") {
+    let listItem = event.target.parentElement; 
+    listItem.remove(); 
+    console.log("deleted")
+  }
+}); 
+
+    
 
 
 
