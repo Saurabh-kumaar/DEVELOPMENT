a05b24f48f40b2e4ac217a93c89470677644bb0e
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-05-10 17:06:33 +0530

    Add

--- JavaScript/Mini-project-js/SIMON/index.js
+++ JavaScript/Mini-project-js/SIMON/index.js
@@ -1,6 +1,8 @@
 
 let gameSeq = []; 
-let userSeq = []; 
+let userSeq = [];  
+
+let btns = ["yellow", "red", "purple", "green"]; 
 
 let started = false; 
 let level = 0;   
@@ -15,16 +17,36 @@ document.addEventListener("keypress", function () {
     levelUp(); 
   }
 }); 
+ 
+function btnFlash(btn) { 
+  btn.classList.add("flash"); 
+  setTimeout(function () {
+    btn.classList.remove("flash"); 
+  }, 500); 
+
+} 
+
 
 function levelUp() {
   level++; 
   h2.innerText = `Level ${level}`;  
 
-   
-  
+  let randIdx = Math.floor(Math.random() * 3); 
+  let randColor = btns[randIdx]; 
+  let randBtn = document.querySelector(`.${randColor}`); 
+  console.log(randIdx); 
+  console.log(randBtn); 
+  console.log(randColor); 
+  btnFlash(randBtn); 
+
 }
+ 
+function btnPress{
 
+   
 
+}
+ 
 
 
 
