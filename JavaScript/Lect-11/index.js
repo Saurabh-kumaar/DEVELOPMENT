3d2c3c7a73c898e52b8325492577dfa05aa44451
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-05-13 08:50:01 +0530

    Add

--- JavaScript/Lect-11/index.js
+++ JavaScript/Lect-11/index.js
@@ -14,25 +14,36 @@
 
 // ----------------------------------- 
 
-function hello() {
-  console.log("inside hello fnx"); // 3
-  console.log("hello");   // 4
-}
-
-function demo() {
-  console.log("calling hello fnx");  // 2
-  hello(); 
-}
-
-console.log("calling demo fnx");  // 1
-demo(); 
-console.log("done, bye!"); // 5
+              // JS is thread 
 
 
+// function hello() {
+//   console.log("inside hello fnx"); // 3
+//   console.log("hello");   // 4
+// }
 
+// function demo() {
+//   console.log("calling hello fnx");  // 2
+//   hello(); 
+// }
 
+// console.log("calling demo fnx");  // 1
+// demo(); 
+// console.log("done, bye!"); // 5
 
+// --------------------------------------- 
 
+// let a = 23; 
+// console.log(a); 
+// let b = 12; 
+// console.log(b); 
+// console.log(a+b); 
+
+// ----------------------------------------
+
+setTimeout(() => {
+  console.log("apna college"); 
+}, 2000);
 
 
 
