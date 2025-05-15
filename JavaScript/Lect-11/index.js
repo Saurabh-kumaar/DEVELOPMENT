963e9aa95bef9da9ececdc5659c0c5d2e5568a9e
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-05-15 09:18:05 +0530

    Add

--- JavaScript/Lect-11/index.js
+++ JavaScript/Lect-11/index.js
@@ -33,6 +33,8 @@
 
 // --------------------------------------- 
 
+        // js in single threaded 
+
 // let a = 23; 
 // console.log(a); 
 // let b = 12; 
@@ -41,9 +43,135 @@
 
 // ----------------------------------------
 
-setTimeout(() => {
-  console.log("apna college"); 
-}, 2000);
+          // js single threaded 
+
+// setTimeout(() => {
+//   console.log("apna college"); 
+// }, 2000); 
+
+// setTimeout(() => {
+//   console.log("shardha maam"); 
+// }, 1000);
+
+
+// console.log("hello..............");
+
+// ------------------------------------------
+ 
+        // Callback Hell 
+
+// h1 = document.querySelector("h1"); 
+
+// // h1.style.color = "pink"; 
+
+// function changeColor(color, delay, nextchangeColor) {
+//   setTimeout(() => {   // it is change the color and do delay work 
+//     h1.style.color = color;  
+//     if(nextchangeColor) nextchangeColor(); 
+//   }, delay); 
+// }
+
+// changeColor("red", 1000, () =>{
+//   changeColor("brown", 2000, () =>{
+//     changeColor("green", 5000); 
+//   }); 
+// }); 
+
+
+// changeColor("pink", 3000); 
+// changeColor("magenta", 5000); 
+
+  
+// 2nd method .callback :- 
+
+// setTimeout(() => {
+//   h1.style.color = "red"; 
+// }, 1000); 
+
+// setTimeout(() => {
+//   h1.style.color = "yellow"; 
+// }, 2000); 
+
+// setTimeout(() => {
+//   h1.style.color = "green"; 
+// }, 5000); 
+
+
+// ========================================================================= 
+
+      // Setting up for Promises 
+
+
+// function savetoDb(data, success, failure) {
+//   let internetSpeed = Math.floor(Math.random() * 10) + 1; 
+//   if(internetSpeed > 4) {  
+//     success(); 
+//    } else { 
+//     failure(); 
+//    }
+// }
+
+// savetoDb(
+//   "hello brothers",
+//   () => {
+//     console.log("success : your data was saved");  
+//     savetoDb
+//       ("hello world",
+//          () => {
+//           console.log("success2 : data2 saved"); 
+
+//         }, 
+//         () => {
+//           console.log("failure2 :  weak connection"); 
+//         }
+//       );
+//     }, 
+//     () => {
+//       console.log("failure : weak connection, data not saved"); 
+//   }
+// ); 
+
+// =============================================================================== 
+
+          // Refactoring with Promises 
+          
+
+function savetoDb(data) {
+  return new Promise((resolve, reject) => {
+    let internetSpeed = Math.floor(Math.random () * 10) + 1; 
+    if(internetSpeed > 4) {
+      resolve("success : data was saved "); 
+    } else {
+      reject("failure : weak connection")
+    }
+  });
+}
+ 
+// savetoDb("hello guys ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
