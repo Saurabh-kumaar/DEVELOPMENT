6ca8ccfb9f8ae42b8ddef46b3fcf67a640d48a6d
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-05-16 22:02:36 +0530

    Add

--- JavaScript/Lect-12/index.js
+++ JavaScript/Lect-12/index.js
@@ -76,7 +76,7 @@
  
 let jsoRes = '{"fact":"People who are allergic to cats are actually allergic to cat saliva or to cat dander. If the resident cat is bathed regularly the allergic people tolerate it better.","length":165}';
 console.log(jsoRes); 
-
+dgdhh git
 
 
 
