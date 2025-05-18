7c7dff1a43d7121c0ddd65c8c11bc035eb6ede59
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-05-18 23:05:56 +0530

    Add

--- JavaScript/Lect-12/index.js
+++ JavaScript/Lect-12/index.js
@@ -91,10 +91,29 @@
 
 let url = "https://catfact.ninja/fact"; 
  
-fetch(url); 
+fetch(url)
+    .then((res) => {
+        console.log(res);  
+        return res.json();
+        })
+    .then((data) => {
+        console.log(data.fact);
+        return fetch(url); 
+    })
+    .then((res) => {
+        return res.json9(); 
+    })
+    .then((data2) => {
+        console.log("data2" )
+    })
+    .catch((err) => {
+        console.log("ERROR -", err); 
+    });
 
 
-ajk 
+
+
+
 
 
 
