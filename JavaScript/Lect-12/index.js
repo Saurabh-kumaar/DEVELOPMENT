1595c03802fb396c5f3f30cb113fb93ebadaaa2a
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-05-23 20:21:20 +0530

    Add

--- JavaScript/Lect-12/index.js
+++ JavaScript/Lect-12/index.js
@@ -89,22 +89,60 @@
 // ------------------------
 
 
-let url = "https://catfact.ninja/fact"; 
+        // our first api 
+
+
+// let url = "https://catfact.ninja/fact"; 
  
-fetch(url) 
-    .then((res) => {
-        console.log(res);
-        return res.json(); 
-    }) 
-    .then((data) => {
-        console.log(data.fact); 
-    })
-    .catch((err) => {
-        console.log("ERROR -", err); 
-    }); 
+// fetch(url) 
+//     .then((res) => {
+//          return res.json(); 
+//     }) 
+//     .then((data) => {
+//         console.log("data1 = ",data.fact); 
+//         return fetch(url); 
+//     })
+//     .then((res) => {
+//         return res.json(); 
+//     })
+//     .then((data2) => {
+//         console.log("data2 =", data2.fact); 
+//     })
+//     .catch((err) => {
+//         console.log("ERROR -", err); 
+//     }); 
+
+// ========================================================================== 
 
 
+let url = "https://catfact.ninja/fact";
 
+        // help of fetch 
+// async function getfacts() {  
+//     try {
+//         let res = await fetch(url); 
+//         let data = await res.json(); 
+//         console.log(data.fact); 
+  
+//         let res2 = await fetch(url); 
+//         let data2 = await res2.json(); 
+//         console.log(data2.fact);  
+//     } catch (e) {
+//         console.log("error -", e); 
+//     } 
+//     console.log("byeee"); 
+// }
+
+    // same code help of axios 
+
+async function getfacts() {
+    try{
+        let res = await axios.get(url); 
+        console.log(res.data.fact); 
+    } catch (e) {
+        console.log("error -", e); 
+    }
+}
 
 
 
