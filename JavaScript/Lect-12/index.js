1b00b51e3c137079f99afa0c7ed3448acc9b89db
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-05-24 22:06:31 +0530

    Add

--- JavaScript/Lect-12/index.js
+++ JavaScript/Lect-12/index.js
@@ -259,7 +259,7 @@ btn.addEventListener("click", async () => {
     list.innerText = "";
 
     for(col of colArr) {
-        console.log(col.name);  adf 
+        console.log(col.name);  iour
 
         let li = document.createElement("li"); 
         li.innerText = col.name; 
