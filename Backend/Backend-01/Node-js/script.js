3e0716799f7385620291361bfbd8f4700607bc08
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-06-04 18:24:14 +0530

    Add

--- Backend/Backend-01/Node-js/script.js
+++ Backend/Backend-01/Node-js/script.js
@@ -64,28 +64,8 @@ import { g, mul, sum, PI} from './Math.js';
 console.log(sum(1, 2)); 
 console.log("PI:", PI);
 console.log("mul:", mul(4, 2));
-console.log("Gravity:", g);   
+console.log("Gravity:", g);  
 
 
 
-
-cfhj
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+jhk
