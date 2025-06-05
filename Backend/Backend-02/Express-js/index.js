6b75641690b292673b5744d340ad9624e918ffbe
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-06-05 23:12:13 +0530

    Add

--- Backend/Backend-02/Express-js/index.js
+++ Backend/Backend-02/Express-js/index.js
@@ -9,11 +9,31 @@ app.listen(port, () => {
   console.log(`app is listening on port ${port}`); 
 });
 
+app.use((req, res) => {
+  // console.log(req); 
+  console.log("request received");
+  // res.send({
+  //   name: "apple", 
+  //   color: "red",
+  // });  
 
+  let code = "<h1>Fruits</h1> <ul><li>apple</li><li>banana</li></ul>"; 
+  res.send(code); 
+});
+ 
+app.get("/", (req, res) => {
+  res.send("hello i am root new version"); 
+})
+ 
+app.get("/", (req, res) => {
+  res.send("you contacted apple path"); 
+})
 
+app.get("/", (req, res) => {
+  res.send("you contacted orange path"); 
+})
 
-
-
-
-
+app.get("/", (req, res) => {
+  res.send("this path does not work "); 
+}); 
 
