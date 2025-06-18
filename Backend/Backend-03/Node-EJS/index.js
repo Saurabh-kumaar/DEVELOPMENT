7e09b882309cc9dcfd34416f170fb123ac7731ce
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-06-18 22:51:38 +0530

    Add

---
+++ Backend/Backend-03/Node-EJS/index.js
@@ -0,0 +1,17 @@
+
+const express = require("express"); 
+const app = express(); 
+
+const port = 8080; 
+
+app.set("view engine", "ejs"); 
+
+app.get("/", (req, res) => {
+  // res.send("this is home"); 
+  res.render("home.ejs"); 
+}); 
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`); 
+}); 
+
