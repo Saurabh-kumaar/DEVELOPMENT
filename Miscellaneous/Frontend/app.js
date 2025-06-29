3583795a2cd9e9835dfc638fb62bc7291c52a48b
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-06-29 23:05:00 +0530

    Add

---
+++ Miscellaneous/Frontend/app.js
@@ -0,0 +1,17 @@
+ 
+let arr = [1, 3, 5]; 
+let arr2 = [5, 2, 8]; 
+arr.sayHello = () => {
+  console.log("hello brother!, i am arr"); 
+}; 
+
+arr2.sayHello = () => {
+  console.log("hello brother!, i am arr"); 
+}; 
+
+
+
+
+
+
+ 
