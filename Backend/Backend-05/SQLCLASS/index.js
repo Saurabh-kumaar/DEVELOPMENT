a7ff03a8693e63a395b5a19ee9a19ada2ed08553
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-07-30 23:23:51 +0530

    Add

--- Backend/Backend-05/SQLCLASS/index.js
+++ Backend/Backend-05/SQLCLASS/index.js
@@ -83,7 +83,7 @@ app.get("/user/:id/edit", (req, res) => {
     res.send("some error in DB");
   }
 });
-ljakdjfie 
+ljakdjfie kjhh
 // UPDAte (DB) ROUTE 
 app.patch("/user/:id", (req, res) => {
   let { id } = req.params; 
