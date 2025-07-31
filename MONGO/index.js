9952b691fd6d6e5b9d4b8746029a244d9d512222
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-07-31 21:28:19 +0530

    Add

---
+++ MONGO/index.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://127.0.0.1:27017/test'); // same code like under
+
+main()
+  .then(()  =>  {
+    console.log("connection successful");
+  })
+  .catch((err) => console.log(err)); 
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+}
+
+const userSchema = new mongoose.Schema({  // user schema create
+  name: String, 
+  email: String, 
+  age: Number,
+});
+
+const User = mongoose.model("User", userSchema);  // schema ko db collection me convert kar rhe hai
+
+const user1 = new User({
+  name: "Adam",
+  email: "adam@yahoo.in",
+  age: 49,
+}); 
+
+user1.save(); 
+ 
+
+
+
+
+
+
