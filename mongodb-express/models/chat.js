eb98877c01c48678fd98e92d11350605aa7de4d8
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-08-04 20:50:17 +0530

    Add

---
+++ mongodb-express/models/chat.js
@@ -0,0 +1,30 @@
+
+const mongoose = require("mongoose"); 
+
+const chatSchema = new mongoose.Schema({
+    from : {
+      type: String,
+      required: true 
+    },
+    to : {
+      type: String, 
+      required: true
+    }, 
+    msg: {
+      type: String, 
+      maxLength: 50
+    },
+    created_at: {
+      type: Date, 
+      required: true 
+      
+    }
+})
+
+
+
+
+
+
+
+
