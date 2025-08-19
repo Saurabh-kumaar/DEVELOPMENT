9f3133f3fd50479b21586db071c392c0e019c2ce
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-08-19 21:53:33 +0530

    Add

--- Backend/Backend-06/Middlewares/app.js
+++ Backend/Backend-06/Middlewares/app.js
@@ -2,23 +2,34 @@ const express = require("express");
 const app = express(); 
 
 
-app.use((req, res,next) => {
-  console.log("Hi, I'm 1st middleware"); 
-  // res.send("now it is done"); 
-  next(); 
-});
+// app.use((req, res,next) => {
+//   console.log("Hi, I'm 1st middleware"); 
+//   // res.send("now it is done"); 
+//   next(); 
+// });
 
-
-app.use((req, res, next) => {
-  console.log("Hii, I'm 2nd middleware");
-  next(); 
-}); 
+// app.use((req, res, next) => {
+//   console.log("Hii, I'm 2nd middleware");
+//   next(); 
+// }); 
  
 
-app.use((req, res, next) => {
-  console.log("kya bolu");
-  next(); 
+const checkToken = (req, res, next) => {
+  let { token } = req.query; 
+  if (token === "giveaccess") {
+    next(); 
+  }
+  res.send("ACCESS DENIED!")
+}; 
+
+app.get("/wrong", (req, res) => {
+  abcd = abcd; 
 })
+
+app.get("/api", checkToken, (req, res) => {
+  res.send("some data"); 
+}); 
+
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root."); 
@@ -29,11 +40,27 @@ app.get("/random", (req, res) => {
   res.send("this is a random page"); 
 }); 
 
+// logger - morgan 
+// app.use((req, res, next) => {
+//   req.time = Date.now(); 
+//   console.log(req.method, req.hostname, req.path, req.time); 
+//   next(); 
+// });
+
+// 404
+app.use((req, res) => {
+  res.send("Page Not Found"); 
+}); 
+
+
+
+
 app.listen(8080, () => {
   console.log("server listening to port 8080"); 
 }); 
 
-lsjg 
+
+
 // ========================================================
 
 // // ------- Here i created many middleware 
