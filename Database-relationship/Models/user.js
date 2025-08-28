078760db3302ae07ad92db78f3afdb96ce254815
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-08-28 23:21:49 +0530

    Add

---
+++ Database-relationship/Models/user.js
@@ -0,0 +1,65 @@
+
+const mongoose = require("mongoose"); 
+const { Schema } = mongoose;
+
+main() 
+  .then(() => console.log("connection successful"))
+  .catch((err) => console.log(err)); 
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/relationDemo");
+}
+
+const userSchema = new Schema({
+  username: String,                         // child 
+  addresses: [
+    {
+      _id: false,
+      location: String, 
+      city: String,
+    },
+  ],
+}); 
+lidsjf 
+
+const User = mongoose.model("User", userSchema); 
+
+const addUsers = async () => {
+  let user1 = new User({
+    username: "sherlokhomes",
+    addresses: [
+      {
+        location: "221B Baker Street",
+        city: "London",
+      },
+    ],
+  });
+
+  user1.addresses.push({ location: "P32 wallStreet", city: "London"});
+  let result = await user1.save(); 
+  console.log(result);
+}; 
+
+addUsers(); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
